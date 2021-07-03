84d7a20ddcd1bce3457ba91d4c7af6f2f4e00328
ryo0705s <inspire.sasaki15@gmail.com>
2021-07-03 16:51:26 +0900

    index.tsxの中身を作成

---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1>Letter From You & Me(仮)</h1>
+    <ul>
+      <li>ログイン</li>
+      <li>ユーザー作成</li>
+      <li>みんなの投稿</li>
+    </ul>
+    <h2>サイトについて</h2>
+    <p>
+      このサイトは”手紙を通じて心の安らぎを感じてほしい”そんな思いから生まれました。
+      コロナにより人々の生活は大きく変わりました。
+      日々不安や悩みを感じる事が多くなったように思います。
+      そんな人たちの力になれたら嬉しいです。
+    </p>
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </p>
+  </Layout>
+);
+
+export default IndexPage;
