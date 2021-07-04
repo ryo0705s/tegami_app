84e6f732be75b71a2166a2a2193f63ce8f5df827
ryo0705s <inspire.sasaki15@gmail.com>
2021-07-05 01:22:37 +0900

    ログインページ仮完成

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,19 +1,31 @@
 import { TextField } from "@material-ui/core";
 import React from "react";
 import Layout from "../components/layout";
+import styles from "../components/login.module.scss";
 
 const login: React.FC = () => {
   return (
     <Layout>
-      <p>メール</p>
-      <TextField />
-      <p>パスワード</p>
-      <TextField />
-      <p>グーグルアカウントでログイン</p>
-      <TextField />
-      <div>ゲストログイン</div>
-      <div>パスワードを忘れた</div>
-      <div>アカウントを作成する</div>
+      <h2>ログイン</h2>
+      <div className={styles.login}>
+        <div>
+          <span>メール</span>
+          <TextField type="email" />
+        </div>
+        <br />
+        <div>
+          <span>パスワード</span>
+          <TextField type="password" />
+        </div>
+        <br />
+        <p>グーグルアカウントでログイン</p>
+        <br />
+        <p>ゲストログイン</p>
+        <br />
+        <p>パスワードを忘れた</p>
+        <br />
+        <p>アカウントを作成する</p>
+      </div>
     </Layout>
   );
 };
