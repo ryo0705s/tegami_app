eebeb65d657d896f15e9c9c491f75e9dba298e75
ryo0705s <inspire.sasaki15@gmail.com>
2021-07-05 02:11:36 +0900

    ユーザー作成画面仮完成

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,18 +1,26 @@
-import { TextField } from "@material-ui/core";
+import { Button, TextField } from "@material-ui/core";
 import React from "react";
 import Image from "next/image";
 import Layout from "../components/layout";
+import PhotoCameraIcon from "@material-ui/icons/PhotoCamera";
+import styles from "../components/user.module.scss";
 
 const user: React.FC = () => {
   return (
     <Layout>
-      <Image src="/sasaki.jpeg" width="20" height="20" />
+      <Image src="/sasaki.jpeg" width={100} height={100} />
+      <PhotoCameraIcon />
       <p>レターネーム</p>
       <TextField />
-      <p>備考</p>
-      <TextField />
-      <p>投稿一覧</p>
-      <TextField />
+      <p>コメント</p>
+      <TextField multiline variant="outlined" />
+      <p>
+        <Button variant="contained" color="primary">
+          作成
+        </Button>
+      </p>
+      <br />
+      <div className={styles.posts}>投稿一覧</div>
     </Layout>
   );
 };
