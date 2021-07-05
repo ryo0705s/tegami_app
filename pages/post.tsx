20babc6899bc3294b22cc9d20e6ee6a4f62669cc
ryo0705s <inspire.sasaki15@gmail.com>
2021-07-05 20:50:31 +0900

    投稿詳細画面仮作成

--- pages/post.tsx
+++ pages/post.tsx
@@ -7,12 +7,22 @@ import Layout from "../components/layout";
 const post: React.FC = () => {
   return (
     <Layout>
-      <Image src="/sasaki.jpeg" width="20" height="20" />
-      <p>説明</p>
-      <TextField />
-      <p>コメント</p>
-      <TextField />
+      <Image src="/letter1.jpg" width="400" height="500" />
       <ThumbUpAltIcon />
+      <p>説明</p>
+      <TextField
+        multiline
+        variant="outlined"
+        fullWidth
+        value="年賀状というものは面白いものである。普通手紙は１対１のやりとりだが、１対２にも３にもなる。かくゆうこの年賀状も、私の親友、そして嫁、そして赤ん坊、の合作である。これは2019年に私がもらったものであるが・・・・・・"
+      />
+      <p>コメント</p>
+      <TextField
+        multiline
+        variant="outlined"
+        fullWidth
+        value="面白いですね！"
+      />
     </Layout>
   );
 };
--- pages/posts.tsx
+++ pages/posts.tsx
@@ -2,25 +2,51 @@ import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 import Layout from "../components/layout";
+import styles from "../components/posts.module.scss";
 
 const posts: React.FC = () => {
   return (
     <Layout>
-      <ul>
+      <ul className={styles.posts}>
         <li>
           <Link href="/post">
-            <Image
-              src="/kumamotofood_dekomikan-00115.jpg"
-              width="40"
-              height="40"
-              alt="test"
-            />
+            <Image src="/letter1.jpg" width={100} height={100} alt="test" />
           </Link>
           <p>投稿１</p>
         </li>
         <li>
-          <Image src="/sasaki.jpeg" width="40" height="40" alt="test" />
+          <Image src="/sasaki.jpeg" width={100} height={100} alt="test" />
           <p>投稿２</p>
+        </li>
+        <li>
+          <Link href="/post">
+            <Image
+              src="/kumamotofood_dekomikan-00115.jpg"
+              width={100}
+              height={100}
+              alt="test"
+            />
+          </Link>
+          <p>投稿3</p>
+        </li>
+        <li>
+          <Image src="/sasaki.jpeg" width={100} height={100} alt="test" />
+          <p>投稿4</p>
+        </li>
+        <li>
+          <Link href="/post">
+            <Image
+              src="/kumamotofood_dekomikan-00115.jpg"
+              width={100}
+              height={100}
+              alt="test"
+            />
+          </Link>
+          <p>投稿5</p>
+        </li>
+        <li>
+          <Image src="/sasaki.jpeg" width={100} height={100} alt="test" />
+          <p>投稿6</p>
         </li>
       </ul>
     </Layout>
