2572f0a37fafd2d678d3e49a3c249619019f4e63
ryo0705s <inspire.sasaki15@gmail.com>
2021-07-11 17:40:10 +0900

    アカウントを作成してfirebaseにログイン成功

--- firebase.ts
+++ firebase.ts
@@ -1,24 +1,24 @@
 import firebase from "firebase/app";
-import firebaseAuth from "firebase/auth";
+import "firebase/auth";
 
-const firebaseConfig = {
-  apiKey: process.env.NEXT_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.NEXT_FIREBASE_DATABASE_URL,
-  projectId: process.env.NEXT_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_FIREBASE_MESSAGEING_SENDER_ID,
-  appId: process.env.NEXT_FIREBASE_APP_ID,
-};
-console.log(process.env.NEXT_FIREBASE_API_KEY);
-firebase.initializeApp(firebaseConfig);
-const auth = firebase.auth();
-firebase.analytics();
-
-firebase
-  .auth()
-  .createUserWithEmailAndPassword(email, password)
-  .then(() => {})
-  .catch((error) => {
-    error.message;
-  });
+const firebaseApp = firebase.initializeApp({
+  // apiKey: process.env.NEXT_FIREBASE_API_KEY,
+  // authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.NEXT_FIREBASE_DATABASE_URL,
+  // projectId: process.env.NEXT_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_FIREBASE_MESSAGEING_SENDER_ID,
+  // appId: process.env.NEXT_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCNOWL6WQRN0GEVJq7E0cV6TnHsr4PjOSQ",
+  authDomain: "tegami-app.firebaseapp.com",
+  databaseURL: "https://tegami-app.firebaseapp.com",
+  projectId: "tegami-app",
+  storageBucket: "tegami-app.appspot.com",
+  messagingSenderId: "54665515800",
+  appId: "1:54665515800:web:170a2d47862ab9cfd2846d",
+  measurementId: "G-8R7RDZ6DGK",
+});
+console.log(firebaseApp);
+// firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+// firebase.analytics();
--- pages/loginUser.tsx
+++ pages/loginUser.tsx
@@ -1,26 +1,49 @@
 import { Button, TextField } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../components/layout";
 import styles from "../components/login.module.scss";
-
-const loginUser: React.FC = () => {
+import firebase from "firebase/app";
+import "firebase/auth";
+import { auth } from "../firebase";
+const loginUser: React.FC = (props: any) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isLogin, setIsLogin] = useState(false);
   const handleSubmit = () => {
-    
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result: any) => {
+        return result;
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
+  useEffect(() => {});
   return (
     <Layout>
       <h2>ユーザーを作成する</h2>
       <div className={styles.login}>
         <div>
           <span>メール</span>
-          <TextField type="email" value={email} onChange={e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement> => {setEmail(e.target.value)} />
+          <TextField
+            type="email"
+            value={email}
+            onChange={(
+              e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+            ) => setEmail(e.target.value)}
+          />
         </div>
         <br />
         <div>
           <span>パスワード</span>
-          <TextField type="password" value={password} />
+          <TextField
+            type="password"
+            value={password}
+            onChange={(
+              e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>
+            ) => setPassword(e.target.value)}
+          />
         </div>
         <br />
         <Button variant="contained" color="primary" onClick={handleSubmit}>
