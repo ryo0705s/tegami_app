9ad29df5a359c6d70de3e99f5d3a86e78d82afef
ryo0705s <inspire.sasaki15@gmail.com>
2021-07-11 21:39:11 +0900

    アカウント作成後ログインページに遷移設定

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,13 @@ import firebase from "firebase/app";
 import "firebase/auth";
 
 const firebaseApp = firebase.initializeApp({
-  // apiKey: process.env.NEXT_FIREBASE_API_KEY,
-  // authDomain: process.env.NEXT_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: process.env.NEXT_FIREBASE_DATABASE_URL,
-  // projectId: process.env.NEXT_FIREBASE_PROJECT_ID,
-  // storageBucket: process.env.NEXT_FIREBASE_STORAGE_BUCKET,
-  // messagingSenderId: process.env.NEXT_FIREBASE_MESSAGEING_SENDER_ID,
-  // appId: process.env.NEXT_FIREBASE_APP_ID,
+  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_APIKEY,
+  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  // databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   apiKey: "AIzaSyCNOWL6WQRN0GEVJq7E0cV6TnHsr4PjOSQ",
   authDomain: "tegami-app.firebaseapp.com",
   databaseURL: "https://tegami-app.firebaseapp.com",
--- pages/loginUser.tsx
+++ pages/loginUser.tsx
@@ -5,10 +5,12 @@ import styles from "../components/login.module.scss";
 import firebase from "firebase/app";
 import "firebase/auth";
 import { auth } from "../firebase";
+import { useRouter } from "next/router";
+
 const loginUser: React.FC = (props: any) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isLogin, setIsLogin] = useState(false);
+  const router = useRouter();
   const handleSubmit = () => {
     auth
       .createUserWithEmailAndPassword(email, password)
@@ -18,8 +20,10 @@ const loginUser: React.FC = (props: any) => {
       .catch((error) => {
         alert(error.message);
       });
+    setEmail("");
+    setPassword("");
+    router.push("/login");
   };
-  useEffect(() => {});
   return (
     <Layout>
       <h2>ユーザーを作成する</h2>
